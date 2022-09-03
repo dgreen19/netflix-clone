85a1d37f7290b3cf23309eba2303587db499826b
Darryl <green.jr.darryl@gmail.com>
2022-09-03 12:59:33 -0400

    Finished sign up page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,94 @@
-import React from 'react'
+import {  onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import BackgroundImage from "../components/BackgroundImage";
+import Header from "../components/Header";
+import {firebaseAuth} from "../utils/firebase-config"
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+	const [showPassword, setShowPassword] = useState(false);
+	const [formValues, setFormValues] = useState({
+		email: "",
+		password: "",
+	});
+	const handleSignIn = async () => {
+		try {
+			const { email, password } = formValues;
+			await signInWithEmailAndPassword(firebaseAuth, email, password)
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	const navigate = useNavigate();
+
+	//if user already has an existing account will be redirectedk to the Netflix homepage!
+	onAuthStateChanged(firebaseAuth, (currentUser) => {
+		if (currentUser) navigate("/");
+	});
+	return (
+		<Container>
+			
+		</Container>
+	);
 }
+
+const Container = styled.div`
+	position: relative;
+	.content {
+		position: absolute;
+		top: 0;
+		left: 0;
+		background-color: #2929297f;
+		height: 100vh;
+		width: 100vh;
+		display: grid;
+		grid-template-rows: 15vh 85vh;
+		.body {
+			gap: 1rem;
+			.text {
+				gap: 1rem;
+				text-align: center;
+				font-size: 2rem;
+				h1 {
+					padding: 0 25rem;
+				}
+			}
+			.form {
+				display: grid;
+				grid-template-columns: ${({showPassword})=>showPassword ? "1fr 1fr":"2fr 1fr"};
+				width: 60%;
+				input {
+					color: black;
+					border: none;
+					padding: 1.5rem;
+					font-size: 1.2rem;
+					border: 1px solid black;
+					&:focus {
+						outline: none;
+					}
+				}
+				button {
+					padding: 0.5rem 1rem;
+					background-color: #e50914;
+					border: none;
+					color: white;
+
+					cursor: pointer;
+					font-weight: bolder;
+					font-size: 1.05rem;
+				}
+			}
+			button {
+				padding: 0.5rem 1rem;
+				background-color: #e50914;
+				border: none;
+				color: white;
+				border-radius: 0.2rem;
+				cursor: pointer;
+				font-weight: bolder;
+				font-size: 1.05rem;
+			}
+		}
+	}
+`;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import React, { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import BackgroundImage from "../components/BackgroundImage";
 import Header from "../components/Header";
+import {firebaseAuth} from "../utils/firebase-config"
 
 export default function SignUp() {
+	const [showPassword, setShowPassword] = useState(false);
+	const [formValues, setFormValues] = useState({
+		email: "",
+		password: "",
+	});
+	const handleSignIn = async () => {
+		try {
+			const { email, password } = formValues;
+			await createUserWithEmailAndPassword(firebaseAuth, email, password)
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	const navigate = useNavigate();
+
+	//if user already has an existing account will be redirectedk to the Netflix homepage!
+	onAuthStateChanged(firebaseAuth, (currentUser) => {
+		if (currentUser) navigate("/");
+	});
 	return (
-		<Container>
+		<Container showPassword={showPassword}>
 			<BackgroundImage></BackgroundImage>
 			<div className="content">
-			<Header login></Header>
-            <div className="body flex column a-center j-center">
+				<Header login></Header>
+				<div className="body flex column a-center j-center">
 					<div className="text flex column">
 						<h1>Unlimited movies, TV shows and more.</h1>
 						<h4>Watch anywhere. Cancel anytime.</h4>
@@ -20,76 +42,86 @@ export default function SignUp() {
 							type="email"
 							placeholder="Email Address"
 							name="email"
+							value={formValues.email}
+							onChange={(e)=>setFormValues({...formValues,[e.target.name]:e.target.value})}
 						></input>
-						<input
-							type="password"
-							placeholder="Password"
-							name="password"
-						></input>
-						<button>Get Started</button>
+						{showPassword && (
+							<input
+								type="password"
+								placeholder="Password"
+								name="password"
+								value={formValues.password}
+							onChange={(e)=>setFormValues({...formValues,[e.target.name]:e.target.value})}
+							></input>
+						)}
+
+						{!showPassword && <button onClick={()=>setShowPassword(true)}>Get Started</button>}
+							
+						
 					</div>
-					<button>Login</button>
+					<button onClick={handleSignIn}>Sign Up</button>
 				</div>
-				</div>
+			</div>
 		</Container>
 	);
 }
 
 const Container = styled.div`
-position: relative;
-.content{
-	position:absolute;
-	top:0;
-	left: 0;
-	background-color: #2929297f;
-	height: 100vh;
-	width: 100vh;
-	display: grid;
-	grid-template-rows:15vh 85vh;
-	.body {
-		gap: 1rem;
-		.text{
+	position: relative;
+	.content {
+		position: absolute;
+		top: 0;
+		left: 0;
+		background-color: #2929297f;
+		height: 100vh;
+		width: 100vh;
+		display: grid;
+		grid-template-rows: 15vh 85vh;
+		.body {
 			gap: 1rem;
-			text-align: center;
-			font-size: 2rem;
-			h1 {
-				padding: 0 25rem;
-			}
-		}
-		.form {
-			display: grid;
-			width: 60%;
-			input{
-				color: black;
-				border: none;
-				padding: 1.5rem;
-				font-size: 1.2rem;
-				border: 1px solid black;
-				&:focus {
-					outline: none;
+			.text {
+				gap: 1rem;
+				text-align: center;
+				font-size: 2rem;
+				h1 {
+					padding: 0 25rem;
 				}
 			}
-			button{
+			.form {
+				display: grid;
+				grid-template-columns: ${({showPassword})=>showPassword ? "1fr 1fr":"2fr 1fr"};
+				width: 60%;
+				input {
+					color: black;
+					border: none;
+					padding: 1.5rem;
+					font-size: 1.2rem;
+					border: 1px solid black;
+					&:focus {
+						outline: none;
+					}
+				}
+				button {
+					padding: 0.5rem 1rem;
+					background-color: #e50914;
+					border: none;
+					color: white;
+
+					cursor: pointer;
+					font-weight: bolder;
+					font-size: 1.05rem;
+				}
+			}
+			button {
 				padding: 0.5rem 1rem;
-		background-color: #e50914;
-		border: none;
-		color: white;
-		border-radius: 0.2rem;
-		cursor: pointer;
-		font-weight: bolder;
-		font-size: 1.05rem;
+				background-color: #e50914;
+				border: none;
+				color: white;
+				border-radius: 0.2rem;
+				cursor: pointer;
+				font-weight: bolder;
+				font-size: 1.05rem;
 			}
 		}
-		button {
-			padding: 0.5rem 1rem;
-		background-color: #e50914;
-		border: none;
-		color: white;
-		border-radius: 0.2rem;
-		cursor: pointer;
-		font-weight: bolder;
-		font-size: 1.05rem;
-		}
 	}
-}
 `;
