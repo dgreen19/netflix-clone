910dc8441f77a3c6d2923ac764a30162b5017d4e
Darryl <green.jr.darryl@gmail.com>
2022-09-06 21:19:10 -0400

    9/6 commit no major changes. Added bootstrap 5.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import JaneAssessment from "./pages/JaneAssessment";
 import Login from "./pages/Login";
 import Netflix from "./pages/Netflix";
 import Player from "./pages/Player";
 import SignUp from "./pages/SignUp";
 
-
-
 export default function App() {
-  return (
+	return (
 		<BrowserRouter>
 			<Routes>
 				<Route exact path="/login" element={<Login />} />
 				<Route exact path="/signup" element={<SignUp />} />
 				<Route exact path="/player" element={<Player />} />
 				<Route exact path="/" element={<Netflix />} />
+				<Route exact path="/janeassessment" element={<JaneAssessment/>}/>
 			</Routes>
 		</BrowserRouter>
 	);
