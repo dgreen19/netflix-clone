b5092001e196db1c5ddd8938fc0a4937cd424886
Darryl <green.jr.darryl@gmail.com>
2022-09-25 14:37:38 -0400

    Added API URLS for frontend deployment

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const userRoutes = require("./routes/UserRoutes")
-
+const userRoutes = require("./routes/UserRoutes");
 const app = express();
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
 mongoose
-	.connect("mongodb://localhost:27017/netflix-clone", {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-    .then(() => {
-        console.log("DB Connected");
-    });
+	.connect(
+		"mongodb+srv://dgreen91:eras88mm@netflix-clone.ad5gm1m.mongodb.net/?retryWrites=true&w=majority",
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}
+	)
+	.then(() => {
+		console.log("DB Connected");
+	});
 
-app.use("/api/user", userRoutes)
+app.use("/api/user", userRoutes);
 
-app.listen(5000, console.log("server started"));
+app.listen(process.env.PORT || 5000, () => {
+	console.log("server started");
+});
